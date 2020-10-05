68d24b85b452b207cab4ba214fe4f7c309c0458e
digitalroah <digitalroahlegit@gmail.com>
2020-10-05 18:57:10 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// ==============Landing Image slide=================//
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" recent", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " recent";
+}
+
+
+ 
+
+
+    
+
+
+
+    
+
+
+ //================== Javascript Form Validation=================//
+// (function () {
+//    'use strict';
+//    window.addEventListener('load',
+//    function() {
+//        var forms =
+//        document.getElementsByClassName('needs-validation');
+//        var validation =
+//        Array.prototype.filter.call(forms,
+//         function(form) {
+//             form.addEventListener('submit',
+//             function(event) {
+//                 if (form.checkValidity()
+//                 === false) {
+//                     event.preventDefault();
+//                     event.stopPropagation();
+//                 }
+//                 form.classList.add('was-validated');
+//             }, false);
+//         });
+//    }, false);
+// }) ();
